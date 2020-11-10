492a5ace882113148167b5cfc977c0da30e0671d
Bo Yang <bohany@gmail.com>
2020-11-10 01:12:36 -0800

    come on please work this time

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,106 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal style="background-color: transparent; top: 1vh">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          size="md"
+        />
+
+        <q-toolbar-title class="text-weight-bold">The Blemish</q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      overlay
+      behavior="mobile"
+      elevated
+      :width="180"
+      dark
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-2 text-h6"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        >
+          Follow us:
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <div class="logoContainer">
+      <img id="logo" alt="Blemish logo" src="~assets/logoHQ.png" />
+    </div>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Spotify",
+    icon: "headset",
+    link:
+      "https://open.spotify.com/artist/0brQOK8RR5HSVwZJlqWYD7?si=wbvDOmIJQCK9FKWJiiCTkA",
+  },
+  {
+    title: "iTunes",
+    icon: "album",
+    link: "https://apple.co/3lfHLox",
+  },
+  {
+    title: "Instagram",
+    icon: "collections",
+    link: "https://www.instagram.com/blemishofficial/",
+  },
+  {
+    title: "Facebook",
+    icon: "thumb_up",
+    link: "https://www.facebook.com/BlemishOfficial/",
+  },
+  {
+    title: "Youtube",
+    icon: "play_circle_outline",
+    link: "https://www.youtube.com/channel/UClX-QDbKYCtbl1Lj8R1KyVw",
+  },
+];
+
+export default {
+  name: "MainLayout",
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+    };
+  },
+};
+</script>
+<style lang="sass" scoped>
+#logo
+  width: 44vmin
+.logoContainer
+  position: absolute
+  right: 7vmin
+  bottom: 4vmin
+  z-index: 10
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-page class="flex flex-center">
+    <video
+      autoplay
+      src="../assets/vidGorgeous.webm"
+      type="video/webm"
+      class="vidGorgeous"
+      v-if="vidNum === 0"
+      preload="auto"
+      ref="vid0"
+    />
+
+    <video
+      class="vidGhost"
+      src="../assets/vidGhost.mp4"
+      type="video/mp4"
+      v-if="vidNum == 1"
+      preload="auto"
+      ref="vid1"
+      autoplay
+    />
+
+    <video
+      class="vidSayHi"
+      src="../assets/vidSayHi.mp4"
+      type="video/mp4"
+      v-if="vidNum == 2"
+      preload="auto"
+      ref="vid2"
+      autoplay
+    />
+
+    <q-btn
+      round
+      push
+      color="deep-purple-6"
+      icon="skip_next"
+      id="skipBtn"
+      size="1rem"
+      @click="vidNext"
+    />
+    <q-btn
+      glossy
+      outline
+      color="deep-purple-2"
+      icon="play_circle_outline"
+      id="playBtn"
+      size="1.3rem"
+      @click="play"
+      v-if="vidNum === 0 && !gorgeousPlaying"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      vidNum: 0,
+      gorgeousPlaying: false,
+    };
+  },
+  methods: {
+    vidNext() {
+      this.vidNum += 1;
+      if (this.vidNum > 2) {
+        this.vidNum = 0;
+      }
+    },
+    play() {
+      this.gorgeousPlaying = true;
+      this.$refs.vid0.play();
+    },
+  },
+};
+</script>
+<style lang="sass" scoped>
+.vidSayHi
+  position: fixed
+  left: -20
+  top: -45%
+  min-height: 150vh
+
+.vidGorgeous
+  position: fixed
+  left: -20
+  top: 0
+  min-width: 100vw
+  min-height: 100vh
+.vidGhost
+  position: fixed
+  left: -20
+  top: 0
+  min-width: 100vw
+  min-height: 100vh
+#skipBtn
+  position: fixed
+  right: 8vmin
+  bottom: 6vmin
+  z-index: 200
+#pauseBtn
+  position: fixed
+  left: 8vmin
+  bottom: 6vmin
+  z-index: 200
+</style>
+
